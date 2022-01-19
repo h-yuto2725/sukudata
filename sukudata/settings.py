febecc34256477e229a9e1224a215b95c9567ac0
hamada <kcska_1812@kcska.onmicrosoft.com>
2022-01-19 15:59:57 +0900

    urladd

--- sukudata/settings.py
+++ sukudata/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)k8)*v3cc8g+a@c=d6r(xh5ld)k7yikq$6@@^jtal^0e-xeujq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.7colordays.net']
 
 
 
@@ -97,7 +97,7 @@ DATABASES = {
         'NAME': 'sukusukudb',
         'USER': 'root',
         'PASSWORD': 'mysql',
-        'HOST': '127.0.0.1',
+        'HOST': '34.146.142.93',
         'PORT': '3306'
     }
 }
