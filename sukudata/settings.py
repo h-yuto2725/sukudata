f84535689dd3fb67f759ca86cc496c71dff755b9
hamada <kcska_1812@kcska.onmicrosoft.com>
2022-01-20 13:32:40 +0900

    urladd

--- sukudata/settings.py
+++ sukudata/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)k8)*v3cc8g+a@c=d6r(xh5ld)k7yikq$6@@^jtal^0e-xeujq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.7colordays.net']
+ALLOWED_HOSTS = ['.7colordays.net','114.160.83.214']
 
 
 
